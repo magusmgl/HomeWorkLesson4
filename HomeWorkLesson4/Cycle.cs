7bbb45368bd6073e7f3adec831127ad52fcbc889
magus.mgl@yandex.ru <magus_mgl@yandex.ru>
2024-03-28 07:26:59 +0500

    Add project files.

---
+++ HomeWorkLesson4/Cycle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkLesson4
+{
+    public static class Cycle
+    {
+
+        public static void WriteNumberFrom5to1()
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static int SumNumbersToUserNumber()
+        {
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                result += i;
+                Console.WriteLine(result);
+            }
+            return result;
+        }
+
+
+        public static void Print7()
+        {
+            int i = 0;
+            while (i < 100)
+            {
+                i++;
+                if (i % 7 == 0) Console.Write($"{i} ");
+            }
+        }
+
+        public static void Print10NumbersFrom0()
+        {
+            int res = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{res} ");
+                res -= 5;
+            }
+        }
+
+        public static void PrintPowNumbersFrom10To20()
+        {
+            for (int i = 10; i <= 20; i++)
+            {
+                Console.WriteLine(Math.Pow(i, 2));
+            }
+        
+        }
+
+    }
+}
---
+++ HomeWorkLesson4/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HomeWorkLesson4;
+
+
+Cycle.PrintPowNumbersFrom10To20();  
